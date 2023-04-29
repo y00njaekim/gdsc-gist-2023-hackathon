c14a0365a8d9300dedf7c1e764ebd12f5ea2d1d7
KimYoonJae <56385667+y00njaekim@users.noreply.github.com>
2023-04-30 02:55:37 +0900

    Feat: 메인 화면 구성 완료 (#13)

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+        fontFamily: 'Poppins',
         primarySwatch: Colors.blue,
       ),
       home: const LoginScreen(),
